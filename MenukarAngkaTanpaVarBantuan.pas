3195c1b0217db42ea54dd202a8ec99b04542879a
Bisma <111258104+Gyndara@users.noreply.github.com>
2024-10-31 19:25:21 +0700

    Add files via upload

---
+++ MenukarAngkaTanpaVarBantuan.pas
@@ -0,0 +1,17 @@
+program Menukar_Dua_Buah_Angka;
+
+var
+  Angka1,Angka2: integer;
+
+begin
+     writeln('<< MELAKUKAN PERTUKARAN ANGKA TANPA BATUAN VARIABLE>>');
+     write('masukan angka 1 :'); readln(Angka1);
+     write('masukan angka 2 :'); readln(Angka2);
+     Angka1 := Angka1+Angka2;
+     Angka2 := Angka1-Angka2;
+     Angka1 := Angka1-Angka2;
+     writeln('hasil angka 1  :', Angka1);
+     writeln('hasil angka 2  :', Angka2);
+
+     readln;                       
+end.
