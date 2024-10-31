26463396d033ac8d2f3fa12d84164d4a3b720b90
Bisma <111258104+Gyndara@users.noreply.github.com>
2024-10-31 19:22:55 +0700

    Add files via upload

---
+++ VolumeBalok.pas
@@ -0,0 +1,25 @@
+ program Menghitung_Volume_Balok;
+{I.S.: pengguna memasukan panjang, lebar dan tinggi balok}
+{F.S.: menampilkan volume balok}
+//Kamus:
+const
+   Konversi = 2.54;
+var
+   Panjang,Lebar,Tinggi,HasilKonversiPanjang,HasilKonversiLebar,HasilKonversiTinggi,VolumeBalok : real;
+
+//Badan program
+begin
+   writeln('<< MENGHITUNG VOLUME BALOK >>');
+   write('Panjang Balok:'); readln(Panjang);
+   write('Lebar Balok  :'); readln(Lebar);
+   write('Tinggi Balok :'); readln(Tinggi);
+   //perhitungan konversi panjang, lebar, tinggi balok
+   HasilKonversiPanjang := Panjang * Konversi;
+   HasilKonversiLebar   := Lebar   * Konversi;
+   HasilKonversiTinggi  := Tinggi  * Konversi;
+   //perhitungan volume balok
+   VolumeBalok := HasilKonversiPanjang * HasilKonversiLebar * HasilKonversiTinggi;
+   //menampilkan hasil perhitungan volume balok
+   write('Volume Balok :', VolumeBalok:0:2,' cm3');
+   readln;
+end.
