2247e6925f1ce1d9216b0d5d5e8be2ffcf77d0ae
Bisma <111258104+Gyndara@users.noreply.github.com>
2024-10-31 19:20:57 +0700

    Add files via upload

---
+++ GenapGanjil1.pas
@@ -0,0 +1,23 @@
+program MenentukanGenapGanjil;
+{I.S.: Pengguna memasukan sebuah bilangan}
+{I.F.: Menampilkan keterangan "Bilangan Ganjil" atau "Bilangan Genap"}
+
+//Kamus :
+var
+  Bilangan : Integer;
+  Keterangan : String ;
+
+//Badan Program
+begin
+   writeln('<<<< PROGRAM MENENTUKAN BILANGAN GANJIL ATAU GENAP >>>>');
+   write('Masukan Bilangan : ');  readln(Bilangan);
+   //Menentukan Niali Bilangan
+   if (Bilangan mod 2 = 0)
+      then
+         Keterangan := 'Bilangan Genap'
+      else
+         Keterangan := 'Bilangan Ganjil';
+   writeln('Bilangan ', Bilangan , ' ',keterangan);
+
+   readln;
+end.
