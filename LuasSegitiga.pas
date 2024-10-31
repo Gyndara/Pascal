916c7803ed7de74b6d23fefe16c2cfafdbe20bd1
Bisma <111258104+Gyndara@users.noreply.github.com>
2024-10-31 19:26:34 +0700

    Add files via upload

---
+++ LuasSegitiga.pas
@@ -0,0 +1,18 @@
+program Menghitung_Luas_Segitiga;
+{I.S.: pengguna memasukkan alas dan tinggi segitiga}
+{F.S.: menampilkan luas segitiga}
+//Kamus:
+var
+   Alas,Tinggi,LuasSegitiga : real;
+
+//badan program
+begin
+   writeln('<< MENGHITUNG LUAS SEGITIGA >>');
+   write('Alas Segitga  :'); readln(Alas);
+   write('Tinggi Segitga:'); readln(Tinggi);
+   //perhitunga luas segitiga
+   LuasSegitiga := 0.5 * Alas * Tinggi;
+   //menampilkan hasil perhitungan
+   write('Luas Segitiga :', LuasSegitiga:0:2,' cm2');
+   readln;
+end.
